b1acf1e8d42653413a1a3152c193a3e94443390d
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-24 17:31:32 +0530

    Line Comparison-Line is Equal,greater or less than other line

---
+++ UC3_Compare_Line/Compare_Line/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Line_equals
+{
+	public static void Main()
+	{
+		int X, Y;
+		Console.Write("Compare Line\n");
+		Console.Write("------------------------------------------------------");
+		Console.Write("\n\n");
+
+		Console.Write("Enter the value for X coordinate :");
+		X = Convert.ToInt16(Console.ReadLine());
+		Console.Write("Enter the value for Y coordinate :");
+		Y = Convert.ToInt16(Console.ReadLine());
+		int compare_value = X.CompareTo(Y);
+		Console.WriteLine(compare_value);
+		if (compare_value==0)
+		{
+			Console.WriteLine("Both Lines are equal");
+		}
+		else if (compare_value < 0)
+		{
+			Console.WriteLine("X is less than Y");
+		}
+		else if (compare_value > 0)
+		{
+			Console.WriteLine("X is Greater than Y");
+		}
+	}
+}
