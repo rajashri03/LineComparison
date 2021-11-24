d7b6b4f9a2d4088fff10463ade8767dcb68b5161
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-24 17:27:30 +0530

    Line Comparison-Lines are equal or not

---
+++ UC2_Equals/Equals/Line_equals.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Line_equals
+{
+	public static void Main()
+	{
+		int X, Y;
+		Console.Write("Check Equality of 2 Length\n");
+		Console.Write("------------------------------------------------------");
+		Console.Write("\n\n");
+
+		Console.Write("Enter the value for X coordinate :");
+		X = Convert.ToInt16(Console.ReadLine());
+		Console.Write("Enter the value for Y coordinate :");
+		Y = Convert.ToInt16(Console.ReadLine());
+
+		if (X.Equals(Y))
+		{
+			Console.WriteLine("Lines are equal");
+		}
+		else
+		{
+			Console.WriteLine("Lines are not equal");
+		}
+	}
+}
