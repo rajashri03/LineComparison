5a4e14092e40675cc5c4350224f4d0ada8c0c673
rajashri03 <telvekar.rajashri@gmail.com>
2021-11-24 12:14:30 +0530

    Lenght Calculate

---
+++ UC1_LengthCalculate/Length_Calculate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Length {
+    public static void Main()
+    {
+        int x1 = 3;
+        int x2 = 4;
+        int y1 = 4;
+        int y2 = 3;
+
+        Console.Write(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1) , 2)));
+    }
+}
